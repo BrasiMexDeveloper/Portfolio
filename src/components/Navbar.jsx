37821d76564757958f414b941b43460b93b9ea16
Victor Villagrana <victorsoftwareeng@gmail.com>
2024-02-27 19:57:15 -0800

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { UilBars, UilTimes } from '@iconscout/react-unicons';
 import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
@@ -6,33 +6,52 @@ import Logo from '../assets/image/logo.png';
 
 const NavaBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const node = useRef();
+
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click 
+    setIsOpen(false);
+  };
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
 
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClick);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
 
 
   return (
-    <nav className="bg-gray-900 ">
+    <nav className="bg-gray-900" ref={node}>
       <div className="">
-        <div className="relative flex items-center justify-between h-24">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <div className="relative flex items-center justify-around h-24">
+          <div className="absolute inset-y-0 right-5 flex items-center sm:hidden">
             <button onClick={toggleMenu} className="sm:hidden lg:hidden">
               {isOpen ?
-                <UilTimes className='text-4xl ml-2 mt-2' onClick={() => setIsOpen(!isOpen)} size='40' color="#50e0fa" /> :
-                <UilBars className='text-4xl ml-2 mt-2' onClick={() => setIsOpen(!isOpen)} size='40' color="#50e0fa" />
+                <UilTimes className='text-4xl ml-2 mt-2' onClick={() => setIsOpen(!isOpen)} size='50' color="#50e0fa" /> :
+                <UilBars className='text-4xl ml-2 mt-2' onClick={() => setIsOpen(!isOpen)} size='50' color="#50e0fa" />
               }
             </button>
           </div>
-          <div className="flex-1 flex items-center justify-around  ">
-            <div className="flex-shrink-0 flex items-center">
+          <div className="flex-1 flex items-center md:justify-between p-5  ">
+            <div className="flex-shrink-0 ">
               <Link to='/' className="flex items-center">
                 <img className="h-20 w-auto sm:h-24 p-2" src={Logo} alt="Full Stack Developer" />
                 <div className="hover:opacity-80">
                   <h1 className="text-[#50e0fa] md:text-4xl text-xl ml-3">Victor Villagrana</h1>
-                  <p className="text-[#50e0fa] md:text-2xl text-sm ml-3">Full Stack Developer</p>
+                  <p className="text-[#50e0fa] md:text-2xl text-sm ml-3">Full Stack Software Developer</p>
                 </div>
               </Link>
             </div>
@@ -50,13 +69,15 @@ const NavaBar = () => {
       {/* //? Mobile menu, show/hide based on menu state. */}
       <div className={`${isOpen ? '' : 'hidden'} sm:hidden`}>
         <div className="px-2 pt-2 pb-3 space-y-1">
-          <a href="/about" className="mt-1 block px-2 py-1 text-white text-xl font-semibold rounded md:hover:bg-gray-700 sm:mt-0 sm:ml-2 cursor-pointer">About</a>
-          <a href="/contact" className="mt-1 block px-2 py-1 text-white text-xl font-semibold rounded md:hover:bg-gray-700 sm:mt-0 sm:ml-2 cursor-pointer">Contact</a>
-          <a href="/skills" className="mt-1 block px-2 py-1 text-white text-xl font-semibold rounded md:hover:bg-gray-700 sm:mt-0 sm:ml-2 cursor-pointer">Skills</a>
-          <a href="/projects" className="mt-1 block px-2 py-1 text-white text-xl font-semibold rounded md:hover:bg-gray-700 sm:mt-0 sm:ml-2 cursor-pointer">Projects</a>
+          <ScrollLink to="about" smooth={true} className="mt-1 block px-2 py-1 text-white text-xl font-semibold rounded hover:bg-gray-600 hover:text-[#50e0fa] sm:mt-0 sm:ml-2 cursor-pointer">About</ScrollLink>
+          <ScrollLink to="contact" smooth={true} className="mt-1 block px-2 py-1 text-white text-xl font-semibold rounded hover:bg-gray-600 hover:text-[#50e0fa] sm:mt-0 sm:ml-2 cursor-pointer">Contact</ScrollLink>
+          <ScrollLink to="skills" smooth={true} className="mt-1 block px-2 py-1 text-white text-xl font-semibold rounded hover:bg-gray-600 hover:text-[#50e0fa] sm:mt-0 sm:ml-2 cursor-pointer">Skills</ScrollLink>
+          <ScrollLink to="projects" smooth={true} className="mt-1 block px-2 py-1 text-white text-xl font-semibold rounded hover:bg-gray-600 hover:text-[#50e0fa] sm:mt-0 sm:ml-2 cursor-pointer">Projects</ScrollLink>
         </div>
       </div>
     </nav>
+    
+    
   );
 }
 
